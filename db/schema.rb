f15f8c396fa671be6bcc75c9ed74d7c3c39728c9
ninooo29 <kazuki@ninomiya-no-MacBook-Pro.local>
2016-11-08 00:43:12 +0900

    create_users_groups_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107153457) do
+ActiveRecord::Schema.define(version: 20161107153937) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "group_name", null: false
@@ -47,6 +47,15 @@ ActiveRecord::Schema.define(version: 20161107153457) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "users_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_users_groups_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_users_groups_on_user_id", using: :btree
   end
 
 end
