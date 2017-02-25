47f222a8d60f6761691c99c2439d3cb2d32fa928
ninooo29 <kazuki@ninomiya-no-MacBook-Pro.local>
2017-02-25 23:58:23 +0900

    Create model method message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,14 @@ class Message < ApplicationRecord
   belongs_to :group
   validates :body, presence: true
   mount_uploader :image, ImageUploader
+
+  def api_json
+    {
+      name: user.name,
+      created_at: created_at,
+      body: body,
+      image: image.url
+    }
+  end
+
 end
