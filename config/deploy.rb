d85ab4befd65ffdc47421da3ed8db2bf2a0c4d4f
ninooo29 <kazuki@ninomiya-no-MacBook-Pro.local>
2017-05-25 11:20:56 +0900

    edit Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
-lock '<Capistranoのバージョン>'
+lock '3.8.1'
 
-set :application, '自身のアプリケーション名'
-set :repo_url, 'git@github.com:<レポジトリ名>.git'
+set :application, 'chat-space'
+set :repo_url, 'git@github.com:chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<EC2インスタンスのSSH鍵(pem)へのパス>']
+                  keys: ['/Users/kazuki/.ssh/key_pair.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
